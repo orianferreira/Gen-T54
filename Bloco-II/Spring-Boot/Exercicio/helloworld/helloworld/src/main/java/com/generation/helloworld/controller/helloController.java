0e11de3031343498f41334fa8569ed0aff7c43ed
Rian <brianalmeida998@gmail.com>
2022-06-28 10:50:40 -0300

    introdução ao Spring Boot

---
+++ Bloco-II/Spring-Boot/Exercicio/helloworld/helloworld/src/main/java/com/generation/helloworld/controller/helloController.java
@@ -0,0 +1,17 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+public class helloController {
+
+	@GetMapping
+
+	public String hello() {
+		return "Sup my G!";
+	}
+}
